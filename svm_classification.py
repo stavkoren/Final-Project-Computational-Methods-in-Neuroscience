1e25a1b8cd34ca418d47c67eff44a4aed2a78262
yuvalmor <yuvalmor94@gmail.com>
2020-08-14 16:23:12 +0300

    wip

--- svm_classification.py
+++ svm_classification.py
@@ -1,8 +1,13 @@
 import numpy as np
 
+
+
+
 def load_abalone_data():
     data = np.loadtxt("data/abalone_data.txt", dtype=str, delimiter=',')
+    np.random.shuffle(data)
     print(data)
+
 
 
 load_abalone_data()
