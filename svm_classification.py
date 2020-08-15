f638d13015cc6b522c3ebd13f296ecfec20bea17
yuvalmor <yuvalmor94@gmail.com>
2020-08-15 21:25:13 +0300

    Revert "wip"
    This reverts commit 1e25a1b

--- svm_classification.py
+++ svm_classification.py
@@ -1,13 +1,8 @@
 import numpy as np
 
-
-
-
 def load_abalone_data():
     data = np.loadtxt("data/abalone_data.txt", dtype=str, delimiter=',')
-    np.random.shuffle(data)
     print(data)
-
 
 
 load_abalone_data()
